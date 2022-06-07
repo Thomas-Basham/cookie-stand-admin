bbc1a430ef16a86272fe3a250140a160045e6c98
Thomas Basham <Bashamtg@gmail.com>
2022-06-07 16:19:10 -0700

    fixed bug

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
     let newStore = {
       Location: e.target.Location.value,
       minCustomers: e.target.MinimumCustomersPerHour.value,
-      maxCustomers: e.target.MaximumCustomersPerHour.checked,
-      avgCookies: e.target.AverageCookiesPerHour.checked,
+      maxCustomers: e.target.MaximumCustomersPerHour.value,
+      avgCookies: e.target.AverageCookiesPerHour.value,
     };
     setState(newStore);
     event.target.reset();
@@ -31,7 +31,7 @@ export default function Home() {
 }
 function Header() {
   return (
-    <header className="bg-emerald-500 py-4 px-8">
+    <header className="px-8 py-4 bg-emerald-500">
       <h1 className="text-xl text-gray-700">Cookie Stand Admin</h1>
     </header>
   );
@@ -42,7 +42,7 @@ function Main(props) {
     <main className="flex flex-col items-center py-4 pt-6 space-y-8">
       <CookieForm onSubmit={props.formHandler} />
 
-      <p className="text-gray-500 text-sm">Report Table Coming Soon...</p>
+      <p className="text-sm text-gray-500">Report Table Coming Soon...</p>
 
       <DisplayJson storeData={props.storeData} />
     </main>
@@ -51,7 +51,7 @@ function Main(props) {
 
 function Footer() {
   return (
-    <footer className="bg-emerald-500 sticky bott-0 py-4 px-8">
+    <footer className="sticky px-8 py-4 bg-emerald-500 bott-0">
       <p className="text-xs text-gray-700">©2022</p>
     </footer>
   );
@@ -60,86 +60,91 @@ function Footer() {
 function CookieForm(props) {
   return (
     
+    <form
+      onSubmit={props.onSubmit}
+      className="w-full max-w-screen-lg px-5 py-3 rounded bg-emerald-300 "
+    >
+      <h1>Create Cookie Stand</h1>
 
-      <form onSubmit={props.onSubmit} className="w-full max-w-screen-lg bg-emerald-300 rounded px-5 py-3 ">
-        <h1>Create Cookie Stand</h1>
-        <div className="flex flex-wrap ">
-          <div className="w-full px-3">
-            <label
-              className="tracking-wide text-gray-700 text-xs font-bold "
-              htmlFor="Location"
-            >
-              Location
-            </label>
-            <input
-              className="appearance-none placeholder-black block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              Id="Location"
-              type="string"
-              placeholder="Barcelona"
-              required='true'
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap w-full mb-3">
-
-          
-          <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-           
-            <label
-              className="block text-center tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >
-              Minimum Customers Per Hour
-            </label>
-            <input
-              className="appearance-none placeholder-black w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              Id="MinimumCustomersPerHour"
-              type="text"
-              placeholder="2"
-              required='true'
+      
+      <div className="flex flex-wrap ">
         
-            />
-          </div>
-          <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label
-              className="block text-center tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >
-              Maximum Customers Per Hour
-            </label>
-            <input
-              className="appearance-none placeholder-black w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              Id="MaximumCustomersPerHour"
-              type="text"
-              placeholder="4"
-              required='true'
-            />
-          </div>
-          <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label
-              className="block text-center tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >
-              Average Customers Per Sale
-            </label>
-            <input
-              className="appearance-none placeholder-black w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="AverageCookiesPerHour"
-              type="float"
-              placeholder="2.5"
-              required='true'
-            />
-          </div>
-          <div className="h-100 w-full md:w-1/4 ">
-
-
-            <button
-              type="submit"
-              className="h-full block w-full bg-emerald-500 text-gray-700  rounded "
-            >
-              Create
-            </button>
-          </div>
+        <div className="w-full px-3">
+          <label
+            className="text-xs font-bold tracking-wide text-gray-700 "
+            htmlFor="Location"
+          >
+            Location
+          </label>
+          <input
+            className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 placeholder-black border border-gray-200 rounded appearance-none bg-white-200 focus:outline-none focus:bg-white focus:border-gray-500"
+            Id="Location"
+            type="string"
+            placeholder="Barcelona"
+            required="true"
+          />
         </div>
-      </form>
-    
+      </div>
+
+      
+      <div className="flex flex-wrap w-full mb-3">
+        <div className="w-full px-3 mb-6 md:w-1/4 md:mb-0">
+          <label className="block mb-2 text-xs font-bold tracking-wide text-center text-gray-700">
+            Minimum Customers Per Hour
+          </label>
+          <input
+            className="w-full px-4 py-3 leading-tight text-gray-700 placeholder-black border border-gray-200 rounded appearance-none bg-white-200 focus:outline-none focus:bg-white focus:border-gray-500"
+            Id="MinimumCustomersPerHour"
+            type="text"
+            placeholder="2"
+            required="true"
+          />
+        </div>
+        
+        
+        <div className="w-full px-3 mb-6 md:w-1/4 md:mb-0">
+          <label className="block mb-2 text-xs font-bold tracking-wide text-center text-gray-700">
+            Maximum Customers Per Hour
+          </label>
+          <input
+            className="w-full px-4 py-3 leading-tight text-gray-700 placeholder-black border border-gray-200 rounded appearance-none bg-white-200 focus:outline-none focus:bg-white focus:border-gray-500"
+            Id="MaximumCustomersPerHour"
+            type="text"
+            placeholder="4"
+            required="true"
+          />
+        </div>
+
+        
+        <div className="w-full px-3 mb-6 md:w-1/4 md:mb-0">
+          <label className="block mb-2 text-xs font-bold tracking-wide text-center text-gray-700">
+            Average Customers Per Sale
+          </label>
+          <input
+            className="w-full px-4 py-3 leading-tight text-gray-700 placeholder-black border border-gray-200 rounded appearance-none bg-white-200 focus:outline-none focus:bg-white focus:border-gray-500"
+            id="AverageCookiesPerHour"
+            type="float"
+            placeholder="2.5"
+            required="true"
+          />
+        </div>
+
+        
+        <div className="w-full h-100 md:w-1/4 ">
+          <button
+            type="submit"
+            className="block w-full h-full text-gray-700 rounded bg-emerald-500 "
+          >
+            Create
+          </button>
+        </div>
+        
+        
+      </div>
+
+
+      
+    </form>
   );
 }
 
@@ -147,10 +152,13 @@ function DisplayJson(props) {
   console.log(JSON.stringify(props.storeData));
   return (
     <div>
-      {JSON.stringify(props.storeData) == '{"storeData":[]}'
-        ? "Enter Some Data!"
-        : 
-        <p className="tracking-widest text-gray-500 text-sm">{JSON.stringify(props.storeData)}</p>}
+      {JSON.stringify(props.storeData) == '{"storeData":[]}' ? (
+        "Enter Some Data!"
+      ) : (
+        <p className="text-sm tracking-widest text-gray-500">
+          {JSON.stringify(props.storeData)}
+        </p>
+      )}
     </div>
   );
 }
