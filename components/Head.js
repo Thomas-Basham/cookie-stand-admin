d574b9528aa74d7d3268ab3196296e72e8b2e5b0
Thomas Basham <Bashamtg@gmail.com>
2022-06-09 09:30:19 -0700

    refactored

---
+++ components/Head.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Cookie Stand Admin</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="px-8 py-4 bg-emerald-500">
+      <h1 className="text-xl text-gray-700">Cookie Stand Admin</h1>
+    </header>
+  );
+}
