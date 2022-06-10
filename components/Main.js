f28757559ca0e5b17348a0f0fa6325c1b6702f34
Thomas Basham <Bashamtg@gmail.com>
2022-06-09 21:40:36 -0700

    refactor

--- components/Main.js
+++ components/Main.js
@@ -6,7 +6,7 @@ export default function Main(props) {
   return (
     <main className="flex flex-col items-center justify-center py-4 pt-6 space-y-8 place-content-center ">
       
-      {props.resources ?
+      {props.resources < 1 ?
       <>
       <CookieForm onSubmit={props.formHandler} />
       <ReportTable resources={props.resources} deleteResource={props.deleteResource} storeData={props.storeData} />
